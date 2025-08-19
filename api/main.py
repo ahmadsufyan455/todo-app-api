c8c4c42a44b1a88700c888c503624906792643a0
ahmadsufyan455 <ahmadsufyan455@gmail.com>
2025-08-19 21:38:29 +0700

    feat: add root endpoint

--- api/main.py
+++ api/main.py
@@ -1,17 +1,29 @@
 from fastapi import FastAPI
+from mangum import Mangum
 
 from database import engine
 from models import Base
 from routers import auth, todos, admin, user
-from mangum import Mangum
 
 app = FastAPI()
 
 Base.metadata.create_all(bind=engine)
 
+
+@app.get("/")
+async def about():
+    return {
+        "app_name": "To-Do App",
+        "version": "0.0.1",
+        "author": "Fyn",
+        "status": "healthy"
+    }
+
+
 @app.get("/healthy")
 async def healthy():
     return {"message": "healthy"}
+
 
 app.include_router(auth.router)
 app.include_router(todos.router)
