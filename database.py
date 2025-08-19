3270a788c4ae0541efeced6f1d805f048d5e7a59
ahmadsufyan455 <ahmadsufyan455@gmail.com>
2025-08-19 20:54:55 +0700

    chore: add dotenv

--- database.py
+++ database.py
@@ -1,8 +1,10 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
+import os
+from dotenv import load_dotenv
 
-SQLALCHEMY_DATABASE_URL = "postgresql://zero:zerodev@localhost:5432/todo_app"
+SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL")
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
